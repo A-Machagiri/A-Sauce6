a582ff69c053680d1c63650350f89bc931116135
A-Machagiri <adunurimachagiri@gmail.com>
2024-11-18 13:49:51 +0530

    Create Utilities/WebUtilities/WebUtilities.cs

---
+++ Utilities/WebUtilities/WebUtilities.cs
@@ -0,0 +1,56 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
+namespace YourNamespace.Utilities.WebUtilities
+{
+    public class WebUtilities
+    {
+        private IWebDriver _driver;
+
+        public IWebDriver InitializeDriver(string browserType)
+        {
+            switch (browserType.ToLower())
+            {
+                case "chrome":
+                    _driver = new ChromeDriver();
+                    break;
+                case "edge":
+                    _driver = new EdgeDriver();
+                    break;
+                case "firefox":
+                    _driver = new FirefoxDriver();
+                    break;
+                default:
+                    throw new ArgumentException("Browser not supported.");
+            }
+            return _driver;
+        }
+
+        public void NavigateToUrl(string url)
+        {
+            _driver.Navigate().GoToUrl(url);
+        }
+
+        public IWebElement FindElement(By by)
+        {
+            return _driver.FindElement(by);
+        }
+
+        public void Click(By by)
+        {
+            FindElement(by).Click();
+        }
+
+        public void SendKeys(By by, string text)
+        {
+            FindElement(by).SendKeys(text);
+        }
+
+        public void Quit()
+        {
+            _driver.Quit();
+        }
+    }
+}
